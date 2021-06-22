b67116f3281889345d03ea6256c3f91eca13b16b
IambabeJ <afamudojudy@gmail.com>
2021-06-22 14:24:20 +0100

    my second commit

--- app/src/main/java/com/judith/todoapp/MainActivity.kt
+++ app/src/main/java/com/judith/todoapp/MainActivity.kt
@@ -1,29 +1,52 @@
 package com.judith.todoapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.AdapterView
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    val userName : String = "Admin"
-    val password : String = "Admin"
+    val validuserName : String = "Admin"
+    val validpassword : String = "Admin"
 
     private lateinit var userNameInput : EditText
     private lateinit var passwordInput : EditText
     private lateinit var submitButton: Button
 
 
-
-
-
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        userNameInput = findViewById(R.id.userNameInput)
+        passwordInput = findViewById(R.id.passwordInput)
+        submitButton = findViewById(R.id.submitButton)
+
+        submitButton.setOnClickListener {
+            val userName = userNameInput.text.toString()
+            val password = passwordInput.text.toString()
+            login(userName,password)
+        }
+
+
+
 
     }
+    fun login(userName : String, password : String){
+        if(isValidCredentials(userName, password)){
+            val intent : Intent = Intent(this,HomeActivity:: class.java)
+            intent.putExtra("username",userName)
+            startActivity(intent)
+        }else{
+            Toast.makeText(applicationContext,"Wrong Password", Toast.LENGTH_SHORT).show()
+        }
+    }
+    private fun isValidCredentials(userName: String, password: String) : Boolean{
+        return (userName ==validuserName && password == validpassword)
+    }
+
+
 }
